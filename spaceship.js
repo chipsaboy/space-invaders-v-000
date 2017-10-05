e2012ae22eeecfde6b453d356614ecbee7a83859
Nathan Campbell <nathancampbell@outlook.com>
2017-10-05 19:32:56 +0000

    automatically backed up by learn

--- spaceship.js
+++ spaceship.js
@@ -1,12 +1,12 @@
 class Spaceship {
   constructor(name, crewMembers, phasers, shields) {
-    this.name = name;
-    this.crewMembers = crewMembers;
-    this.phasers = phasers;
-    this.shields = shields;
-    this.warpDrive = "disengaged";
-    this.cloaked = false;
-    this.phasersCharge = "uncharged";
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasersCharge = "uncharged"
     this.docked = this.crewMembers.length < 1 ? true : false;
   }
 }
